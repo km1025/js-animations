4691dbe13b04d7511ac468556e8298ad97528765
km1025 <matsuda.kanoko@gmail>
2024-09-02 21:31:16 +0900

    js-animations

--- accordion-anime/work/js/main.js
+++ accordion-anime/work/js/main.js
@@ -1,23 +1,15 @@
 const menus=document.querySelectorAll('.list-item');
 menus.forEach(menu =>{
-  menu.addEventListener('click',(e)=>{
+  menu.addEventListener('click',()=>{
     //親要素を取得
-    const conten=e.currentTarget.nextElementsibling;
-    //子要素を取得
-    const text=conten.children[0];
-    //子要素の高さを取得して親要素に代入
-    const textHeight=text.offsetHeight;
-    conten.style.height=textHeight+'px';
-
-    //ボタンの親要素にopenクラスを付け外し
-    const parentMenu=e.currentTarget.parentNode;
-    parentMenu.classList.toggle('open');
-
+    const content=menu.querySelector('.list-content');
+    
+    content.classList.toggle('open');
     //条件分岐で開閉
-    if(parentMenu.classList.contains('open')){
-      conten.style.heigh=textHeight+'px';
+    if(content.classList.contains('open')){
+      content.style.height=content.scrollHeight+'px';
     }else{
-      conten.style.height=0;
+      content.style.height=0;
     }
   });
 });
