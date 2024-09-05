a812987c8394cf314b73fdf8819c33727790e84b
km1025 <matsuda.kanoko@gmail>
2024-09-05 23:40:39 +0900

    9/5 js.scrollanimation

--- scroll-anime/work/js/app.js
+++ scroll-anime/work/js/app.js
@@ -2,31 +2,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   handleScrollIn();
 });
-//boxes-wrapper内の全ての要素を取得
+
+//boxクラスを持つ全ての要素の取得
 const handleScrollIn = () => {
-  const targets = document.querySelectorAll(".boxes-wrapper");
+  const targets = document.querySelectorAll(".box");
 
   //IntersectionObserverの動作指定
   const options = {
-    root: null,
-    rootMargin: "-20% 0px",
     threshold: [0]
   };
+
 //交差時に実行されるコールバック
   const callback = (entries, observer) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add("active");
-        //一度アニメーションが適用されたら監視を外す
+        entry.target.classList.add("displayed");
+//一度アニメーションが適用されたら監視を外す
         observer.unobserve(entry.target);
       }
     });
     };
+
 //IntersectionObserverのインスタンス化
     const observer = new IntersectionObserver(callback, options);
 
-    target.forEach((target) => {
-      observer.observe(target);
+//監視する対象の登録
+    targets.forEach((box) => {
+      observer.observe(box);
       });
     };
-
